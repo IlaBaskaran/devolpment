ec99229f249540e93be040824e81dbfb73154a21
Monica Li <monica.amanda.li@gmail.com>
2014-02-28 21:35:23 -0500

    Implement basic buttons on Main Activity

--- gen/com/livequake/disastersafetyalert/R.java
+++ gen/com/livequake/disastersafetyalert/R.java
@@ -24,7 +24,13 @@ public final class R {
         public static final int ic_launcher=0x7f020000;
     }
     public static final class id {
-        public static final int action_settings=0x7f080000;
+        public static final int CheckFriends=0x7f080004;
+        public static final int History=0x7f080000;
+        public static final int Notify=0x7f080001;
+        public static final int Settings=0x7f080005;
+        public static final int WatchArea=0x7f080002;
+        public static final int What=0x7f080003;
+        public static final int action_settings=0x7f080006;
     }
     public static final class layout {
         public static final int activity_main=0x7f030000;
@@ -35,7 +41,12 @@ public final class R {
     public static final class string {
         public static final int action_settings=0x7f050001;
         public static final int app_name=0x7f050000;
-        public static final int hello_world=0x7f050002;
+        public static final int checkFriends=0x7f050007;
+        public static final int history=0x7f050002;
+        public static final int notify=0x7f050006;
+        public static final int settings=0x7f050005;
+        public static final int watch=0x7f050003;
+        public static final int what=0x7f050004;
     }
     public static final class style {
         /** 
